bcb0ee7b59d6c932ae4e6db4765d2dc18b47a93e
Admin <Admin@TEB127030>
2023-03-10 08:44:45 +0100

    Add project files.

---
+++ Uppgift 4.5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace uppgift4_5
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv ett heltal");
+            string heltal = Console.ReadLine();
+
+            for (int i = 0; i < heltal.Length; i++)
+            {
+                string tecken = heltal[i].ToString();
+                int siffra = int.Parse(tecken);
+                if (siffra == 9)
+                {
+                    siffra = 0;
+                }
+                else
+                {
+                    siffra = siffra + 1;
+                }
+                Console.WriteLine(siffra);
+            }
+        }
+    }
+}
